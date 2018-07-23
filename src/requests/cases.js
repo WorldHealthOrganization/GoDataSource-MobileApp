1e4e59da1a6073639c0589811b801674ad6fb04b
Ioana.Tudor <ioana.tudor@clarisoft.com>
2018-07-23 16:13:02 +0300

    request for cases of active outbreak

--- src/requests/cases.js
+++ src/requests/cases.js
@@ -1,3 +1,29 @@
 /**
  * Created by florinpopa on 19/07/2018.
  */
+import url from './../utils/url';
+import {handleResponse} from './../utils/functions';
+
+export function getCasesForOutbreakIdRequest(outbreakId, token, callback) {
+    let requestUrl = url.outbreaks + outbreakId + '/cases';
+
+    fetch(requestUrl, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+            'Authorization': token,
+        }
+    })
+        .then((response) => {
+            return handleResponse(response);
+        })
+        .then((response) => {
+            console.log('### getCasesForOutbreakId resposne: ', response);
+            callback(null, response);
+        })
+        .catch((error) => {
+            console.log("*** getCasesForOutbreakId error: ", error);
+            callback(error);
+        })
+}
