34f1dbd6e8b45723c07a3479c2db688a4f9b25b4
Anda <anda.nenu@clarisoft.com>
2018-12-18 12:36:31 +0200

    communicate with native for installationId -- android

--- android/parse-receiver/src/main/java/com/example/parse_receiver/ParseReceiverModule.java
+++ android/parse-receiver/src/main/java/com/example/parse_receiver/ParseReceiverModule.java
@@ -2,12 +2,23 @@ package com.example.parse_receiver;
 
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.modules.core.DeviceEventManagerModule;
+import com.parse.ParseInstallation;
 
 public class ParseReceiverModule extends ReactContextBaseJavaModule {
 
     public ParseReceiverModule(ReactApplicationContext reactContext) {
         super(reactContext);
         ParseReceiver.reactContext = reactContext;
+    }
+
+    @ReactMethod
+    public void initParse() {
+        String installationId = ParseInstallation.getCurrentInstallation().getInstallationId();
+        if (this.getReactApplicationContext() != null) {
+            this.getReactApplicationContext().getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class).emit("onParseInit", installationId);
+        }
     }
 
     @Override
