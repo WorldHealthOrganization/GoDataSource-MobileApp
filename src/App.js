3fe35ae7ff8e85dfa58d8d5491ec0ed6bb26c8c7
alexandru_stahie <alexandru.stahie@clarisoft.com>
2018-12-18 15:50:02 +0200

    wipeData request

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import promise from 'redux-promise';
 import { createLogger } from 'redux-logger';
-import { Platform, DeviceEventEmitter, NativeEventEmitter, NativeModules } from 'react-native';
+import { Platform, DeviceEventEmitter, NativeEventEmitter, NativeModules, AsyncStorage} from 'react-native';
+import {getInternetCredentials} from 'react-native-keychain';
+import {wipeCompleteRequest} from './requests/wipeData'
 
 import appReducers from './reducers';
 import appActions from './actions';
@@ -30,12 +32,67 @@ export default class App {
             ParseNativeModule = DeviceEventEmitter
         }
         ParseNativeModule.addListener('onParseInit', (item) => {
-            console.log('~~~ TODO save installation Id onParseInit ~~~', item)
-        })
-        ParseNativeModule.addListener('onPushReceived', (item) => {
-            console.log('~~~ TODO WIPE onPushReceived ~~~', item)
+            console.log('~~~ TODO save installation Id onParseInit ~~~', item.toString())
+            AsyncStorage.setItem('installationId', item);
         })
 
+        ParseNativeModule.addListener('onPushReceived', (item) => {
+            console.log('~~~ TODO WIPE onPushReceived ~~~', item)
+
+            //Request to server after finish wipe data
+            AsyncStorage.getItem('installationId')
+            .then((installationId) => {
+                console.log('Response installationId', installationId);
+                if (installationId) {
+                    AsyncStorage.getItem('activeDatabase')
+                    .then((activeDatabase) => {
+                        console.log('Response activeDatabase', activeDatabase);
+                        if (activeDatabase) {
+                            AsyncStorage.getItem(activeDatabase)
+                            .then((lastSyncDate) => {
+                                console.log('Response lastSyncDate: ', lastSyncDate);
+                                if (lastSyncDate) {
+                                    lastSyncDate = new Date(lastSyncDate).toUTCString();
+                                    getInternetCredentials(activeDatabase)
+                                    .then((activeDatabaseCredentials) => {
+                                        if (activeDatabaseCredentials) {
+                                            console.log('Response activeDatabaseCredentials ', activeDatabaseCredentials);
+                                            let currentHubConfig = JSON.parse(activeDatabaseCredentials.username);
+                                            if (currentHubConfig && currentHubConfig !== undefined && currentHubConfig.url && currentHubConfig.url !== undefined && currentHubConfig.url.trim().length > 0 && installationId && installationId !== undefined) {
+                                                // console.log ('configHubInfo', currentHubConfig)
+                                                // console.log ('installationId', installationId)
+                                
+                                                wipeCompleteRequest(currentHubConfig.url, installationId, currentHubConfig.clientId, currentHubConfig.clientSecret, (error, response) => {
+                                                    if (error) {
+                                                        console.log ('wipeCompleteRequest error: ', error)
+                                                    }
+                                                    if (response) {
+                                                        console.log ('wipeCompleteRequest response: ', response)
+                                                    }
+                                                })
+                                            }
+                                        }
+                                    })
+                                    .catch((errorActiveDatabaseCredentials) => {
+                                        console.log('Error active database credentials: ', errorActiveDatabaseCredentials);
+                                    })
+                                }
+                            })
+                            .catch((errorLastSyncDate) => {
+                                console.log('Error while getting last sync date: ', errorLastSyncDate);
+                            })
+                        }
+                    })
+                    .catch((errorActiveDatabase) => {
+                        console.log("Error while getting active database: ", errorActiveDatabase);
+                    });
+                }
+            })
+            .catch((errorInstallationId) => {
+                console.log('Error device id: ', errorInstallationId);
+            })
+        })  
+            
         store.subscribe(this.onStoreUpdate);
         store.dispatch(appActions.appInitialized());
     };
---
+++ src/requests/wipeData.js
@@ -0,0 +1,46 @@
+
+import {Platform} from 'react-native';
+import DeviceInfo from 'react-native-device-info';
+import base64 from 'base-64';
+
+export function wipeCompleteRequest(url, installationId, clientId, clientSecret, callback) {
+    if (url && installationId) {
+        let requestUrl = `${url}/devices/wipe-complete`
+    
+        let operatingSystem = Platform.OS
+        let deviceManufacturer = DeviceInfo.getManufacturer()
+        let deviceModel = DeviceInfo.getModel()
+        let deviceName = DeviceInfo.getDeviceName()
+        let deviceDescription = ''
+    
+        // console.log ('operatingSystem: ', operatingSystem)
+        // console.log ('deviceManufacturer: ', deviceManufacturer)
+        // console.log ('deviceModel: ', deviceModel)
+        // console.log ('deviceName: ', deviceName)
+        // console.log ('deviceDescription: ', deviceDescription)
+        // console.log ('requestUrl: ', requestUrl)
+        // console.log ('installationId: ', installationId)
+
+        let deviceInfoStringify = `{"id": "${installationId}", "os": "${operatingSystem}", "manufacturer": "${deviceManufacturer}", "model": "${deviceModel}", "name": "${deviceName}"}`
+        // console.log('deviceInfoStringify: ', deviceInfoStringify)
+
+        fetch( encodeURI(requestUrl), {
+            method: 'POST',
+            headers: {
+                "device-info": deviceInfoStringify,
+                'Authorization': 'Basic ' + base64.encode(`${clientId}:${clientSecret}`),
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            },
+        })
+            .then((response) => {
+                console.log("*** wipeCompleteRequest response: ", response);
+                callback(null, response);
+            })
+            .catch((error) => {
+                console.log("*** wipeCompleteRequest error: ", error);
+                callback(error);
+            })
+    }
+    
+};
