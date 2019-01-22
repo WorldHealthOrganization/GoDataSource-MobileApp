58ee3864ff4d48ed64e31d698dec787af29453e8
alexandru_stahie <alexandru.stahie@clarisoft.com>
2019-01-22 14:22:58 +0200

    default translations

---
+++ src/utils/defaultTranslations.js
@@ -0,0 +1,421 @@
+export default {
+    //navigationDrawer
+    'LNG_NAVIGATION_DRAWER_ACTIVE_OUTBREAK': 'Active outbreak',
+    'LNG_NAVIGATION_DRAWER_FOLLOW_UPS': 'Follow-ups',
+    'LNG_NAVIGATION_DRAWER_CONTACTS': 'Contacts',
+    'LNG_NAVIGATION_DRAWER_CASES': 'Cases',
+    'LNG_NAVIGATION_DRAWER_SYNC_HUB_MANUAL': 'Sync HUB manually',
+    'LNG_NAVIGATION_DRAWER_CHANGE_HUB_CONFIG': 'Change HUB configuration',
+    'LNG_NAVIGATION_DRAWER_LANGUAGES': 'Languages',
+    'LNG_NAVIGATION_DRAWER_LOGOUT': 'Logout',
+    'LNG_LAYOUT_MENU_ITEM_HELP_LABEL': 'Help & support',
+
+
+    //addFollowUpScreen
+    'LNG_ADD_FOLLOW_UP_MODAL_TITLE': 'Add follow-up',
+    'LNG_ADD_FOLLOW_UP_MODAL_FOLLOW_UPS_DATE': 'Follow-up date',
+    'LNG_ADD_FOLLOW_UP_MODAL_SEARCH_CONTACT': 'Search Contact',
+
+
+    //casesFilter
+    'LNG_CASE_FILTER_SCREEN_TITLE': 'Cases filters',
+    'LNG_FILTER_SCREEN_FILTER_TAB': 'Filters',
+    'LNG_FILTER_SCREEN_SORT_TAB': 'Sort',
+
+    'LNG_FILTER_SCREEN_GENDER_LABEL': 'Gender',
+    'LNG_FILTER_SCREEN_AGE_RANGE_LABEL': 'Age range',
+    'LNG_FILTER_SCREEN_AREA_LABEL': 'Area',
+    'LNG_FILTER_SCREEN_CHOOSE_LOCATIONS_MESSAGE': 'Choose one or more locations',
+    'LNG_FILTER_SCREEN_CLASSIFICATION_LABEL': 'Classification',
+    'LNG_FILTER_SCREEN_CHOOSE_CLASSIFICATIONS_MESSAGE': 'Choose one or more classifications',
+
+
+    //followUpFilter
+    'LNG_FOLLOW_UPS_FILTER_SCREEN_TITLE': 'Follow-ups filters',
+    'LNG_CONTACTS_FILTER_SCREEN_TITLE': 'Contacts filters',
+
+
+    //helpFilter
+    'LNG_SIDE_FILTERS_TITLE': 'Filters',
+    'LNG_SIDE_FILTERS_SECTION_SORT_TITLE': 'Sort',
+    'LNG_HELP_ITEMS_FILTER_SCREEN_TITLE': 'Help filters',
+    'LNG_HELP_ITEMS_FILTER_CATEGORY_LABEL': 'Category',
+    'LNG_HELP_ITEMS_FILTER_CATEGORY_MESSAGE_LABEL': 'Choose one or more categories',
+
+
+    //helpScreen
+    'LNG_HELP_ITEMS_LIST_TITLE': 'Help',
+    'LNG_HELP_ITEMS_VIEW_ACTION_LABEL': 'View',
+    'LNG_HELP_ITEMS_SINGLE_SCREEN_VIEW_TITLE_LABEL': 'View Help Item',
+    'LNG_HELP_ITEMS_EMPTY_LIST_MESSAGE': 'There are not help items to display',
+    'LNG_HELP_ITEMS_ITEMS_FOR_MESSAGE': 'items for',
+
+    'LNG_HELP_ITEMS_ADD_MESSAGE': 'Add',
+    'LNG_HELP_ITEMS_EDIT_MESSAGE': 'Edit',
+    'LNG_HELP_ITEMS_VIEW_MESSAGE': 'View',
+
+    'LNG_HELP_ITEMS_FIELD_LABEL_TITLE': 'Title',
+    'LNG_HELP_ITEMS_FIELD_LABEL_CATEGORY': 'Category',
+    'LNG_HELP_ITEMS_FIELD_LABEL_DESCRIPTION': 'Description',
+    'LNG_HELP_ITEMS_FIELD_LABEL_COMMENT': 'Comment',
+    'LNG_HELP_ITEMS_FIELD_LABEL_PAGE': 'Page',
+
+
+    //sortTab
+    'LNG_SORT_SCREEN_ADD_SORT_RULE_MESSAGE': 'Add sort rule',
+    'LNG_SORT_SCREEN_ADD_SORT_CARD_LABEL_TITLE': 'Sort by',
+    'LNG_SORT_SCREEN_CRITERIA_LABEL': 'Sort Criteria',
+    'LNG_SORT_SCREEN_ORDER_LABEL': 'Sort Order',
+
+    'LNG_CONTACT_FIELD_LABEL_FIRST_NAME': 'First Name',
+    'LNG_CONTACT_FIELD_LABEL_LAST_NAME': 'Last Name',
+    'LNG_SIDE_FILTERS_SORT_BY_ASC_PLACEHOLDER': 'Ascending',
+    'LNG_SIDE_FILTERS_SORT_BY_DESC_PLACEHOLDER': 'Descending',
+
+
+    //caseSingleScreen
+    'LNG_CASE_SINGLE_SCREEN_TITLE': 'Cases',
+    'LNG_CASE_SINGLE_SCREEN_PERSONAL_TAB': 'Personal',
+    'LNG_CASE_SINGLE_SCREEN_ADDRESS_TAB': 'Address',
+    'LNG_CASE_SINGLE_SCREEN_EPIDEMIOLOGY_TAB': 'Epidemiology',
+    'LNG_CASE_SINGLE_SCREEN_INVESTIGATION_TAB': 'Investigation',
+
+    'LNG_CASE_SINGLE_SCREEN_ADD_NEW_CASE_TITLE_LABEL': 'Add case',
+    'LNG_CASE_SINGLE_SCREEN_DELETE_CASE_ACTION': 'Delete case',
+
+    'LNG_HOSPITALIZATION_ADD_NEW_HOSPITALIZATION_MESSAGE': 'Add hospitalization date range',
+    'LNG_HOSPITALIZATION_ADD_ANOTHER_HOSPITALIZATION_MESSAGE': 'Add another hospitalization date range',
+    'LNG_ISOLATION_ADD_NEW_ISOLATION_MESSAGE': 'Add isolation date range',
+    'LNG_ISOLATION_ADD_ANOTHER_ISOLATION_MESSAGE': 'Add another isolation date range',
+    'LNG_DOCUMENT_ADD_NEW_DOCUMENT_MESSAGE': 'Add document',
+    'LNG_DOCUMENT_ADD_ANOTHER_DOCUMENT_MESSAGE': 'Add another document',
+
+    'LNG_CASE_FIELD_LABEL_FIRST_NAME': 'First name',
+    'LNG_CASE_FIELD_LABEL_MIDDLE_NAME': 'Middle name',
+    'LNG_CASE_FIELD_LABEL_LAST_NAME': 'Last name',
+    'LNG_CASE_FIELD_LABEL_GENDER': 'Gender',
+    'LNG_CASE_FIELD_LABEL_PHONE_NUMBER': 'Phone number',
+    'LNG_CASE_FIELD_LABEL_OCCUPATION': 'Occupation',
+    'LNG_CASE_FIELD_LABEL_DOB': 'Date of birth',
+    'LNG_CASE_FIELD_LABEL_AGE': 'Age',
+    'LNG_CASE_FIELD_LABEL_RISK_LEVEL': 'Risk level',
+    'LNG_CASE_FIELD_LABEL_DATE_OF_REPORTING': 'Date of reporting',
+    'LNG_CASE_FIELD_LABEL_IS_DATE_OF_REPORTING_APPROXIMATE': 'Date of reporting approximate',
+    'LNG_CASE_FIELD_LABEL_TRANSFER_REFUSED': 'Refused transfer to treatment unit',
+    'LNG_CASE_FIELD_LABEL_RISK_REASON': 'Risk reason',
+
+    'LNG_CASE_FIELD_LABEL_DOCUMENT_TYPE': 'Document Type',
+    'LNG_CASE_FIELD_LABEL_DOCUMENT_NUMBER': 'Document number',
+
+    'LNG_CASE_FIELD_LABEL_CLASSIFICATION': 'Classification',
+    'LNG_CASE_FIELD_LABEL_DATE_OF_ONSET': 'Date of onset',
+    'LNG_CASE_FIELD_LABEL_IS_DATE_OF_ONSET_APPROXIMATE': 'Date of onset approximate',
+    'LNG_CASE_FIELD_LABEL_DATE_BECOME_CASE': 'Date of becoming case',
+    'LNG_CASE_FIELD_LABEL_DATE_OF_INFECTION': 'Date of infection',
+    'LNG_CASE_FIELD_LABEL_OUTCOME': 'Outcome',
+    'LNG_CASE_FIELD_LABEL_DATE_OF_OUTCOME': 'Date of outcome',
+    'LNG_CASE_FIELD_LABEL_DECEASED': 'Deceased',
+    'LNG_CASE_FIELD_LABEL_SAFETY_BURIAL': 'Was Burial Safety Performed?',
+    'LNG_CASE_FIELD_LABEL_DATE_OF_DECEASE': 'Date of decease',
+
+    'LNG_DATE_FIELD_LABEL_FROM': 'From',
+    'LNG_DATE_FIELD_LABEL_TO': 'To',
+    'LNG_DATE_FIELD_LABEL_CENTER_NAME': 'Center Name',
+    'LNG_DATE_FIELD_LABEL_LOCATION': 'Location',
+    'LNG_DATE_FIELD_LABEL_COMMENTS': 'Comments',
+
+
+    //contactSingleScreen
+    'LNG_CONTACT_SINGLE_SCREEN_TITLE': 'Contacts',
+    'LNG_CONTACT_SINGLE_SCREEN_PERSONAL_TAB': 'Personal',
+    'LNG_CONTACT_SINGLE_SCREEN_ADDRESS_TAB': 'Address',
+    'LNG_CONTACT_SINGLE_SCREEN_EXPOSURE_TAB': 'Exposures',
+    'LNG_CONTACT_SINGLE_SCREEN_CALENDAR_TAB': 'Calendar',
+
+    'LNG_CONTACT_SINGLE_SCREEN_ADD_NEW_CONTACT_TITLE_LABEL': 'Add contact',
+    'LNG_CONTACT_SINGLE_SCREEN_DECEASED_CONTACT_ACTION': 'Deceased',
+    'LNG_CONTACT_SINGLE_SCREEN_DELETE_CONTACT_ACTION': 'Delete contact',
+
+    'LNG_ADDRESS_ADD_NEW_ADDRESS_MESSAGE': 'Add address',
+    'LNG_ADDRESS_ADD_ANOTHER_ADDRESS_MESSAGE': 'Add another address',
+    'LNG_EXPOSURE_ADD_NEW_EXPOSURE_MESSAGE': 'Add exposure',
+
+    'LNG_CONTACT_FIELD_LABEL_MIDDLE_NAME': 'Middle name',
+    'LNG_CONTACT_FIELD_LABEL_GENDER': 'Gender',
+    'LNG_CONTACT_FIELD_LABEL_PHONE_NUMBER': 'Phone number',
+    'LNG_CONTACT_FIELD_LABEL_OCCUPATION': 'Occupation',
+    'LNG_CONTACT_FIELD_LABEL_DATE_OF_REPORTING': 'Date of reporting',
+    'LNG_CONTACT_FIELD_LABEL_IS_DATE_OF_REPORTING_APPROXIMATE': 'Date of reporting approximate',
+    'LNG_CONTACT_FIELD_LABEL_RISK_LEVEL': 'Risk level',
+    'LNG_CONTACT_FIELD_LABEL_RISK_REASON': 'Reason',
+
+
+    //exposureScreen
+    'LNG_RELATIONSHIP_SCREEN_EDIT_EXPOSURE_TITLE': 'Edit exposure',
+    'LNG_RELATIONSHIP_SCREEN_ADD_EXPOSURE_TITLE': 'Add exposure',
+
+    'LNG_RELATIONSHIP_LABEL_CHOOSE_CASE_OR_EVENT': 'Choose a case/event',
+    'LNG_RELATIONSHIP_LABEL_DATE_OF_LAST_CONTACT': 'Date of last contact',
+    'LNG_RELATIONSHIP_LABEL_IS_CONTACT_DATE_ESTIMATED': 'Is contact date estimated?',
+    'LNG_RELATIONSHIP_LABEL_CERTAINTY_LEVEL': 'Certainty level',
+    'LNG_RELATIONSHIP_LABEL_EXPOSURE_TYPE': 'Exposure type',
+    'LNG_RELATIONSHIP_LABEL_EXPOSURE_FREQUENCY': 'Exposure frequency',
+    'LNG_RELATIONSHIP_LABEL_EXPOSURE_DURATION': 'Exposure duration',
+    'LNG_RELATIONSHIP_LABEL_RELATION': 'Relation',
+    'LNG_RELATIONSHIP_LABEL_RELATIONSHIPS': 'Relationships',
+    'LNG_RELATIONSHIP_LABEL_CLUSTER': 'Cluster',
+    'LNG_RELATIONSHIP_LABEL_COMMENT': 'Comment',
+
+
+    //followUpsSingleScreen
+    'LNG_FOLLOW_UPS_SINGLE_SCREEN_TITLE': 'Follow-ups',
+    'LNG_FOLLOW_UPS_SINGLE_SCREEN_DETAILS_TAB': 'Details',
+    'LNG_FOLLOW_UPS_SINGLE_SCREEN_QUESTIONNAIRE_TAB': 'Questionnaire',
+
+    'LNG_FOLLOW_UPS_SINGLE_SCREEN_ACTION_MISSING': 'Missing',
+    'LNG_FOLLOW_UPS_SINGLE_SCREEN_ACTION_DELETE': 'Deleted',
+    'LNG_FOLLOW_UPS_SINGLE_SCREEN_ACTION_DECEASE': 'Deceased',
+
+    'LNG_DIALOG_ALERT_MESSAGE_ARE_YOU_SURE_YOU_WANT_TO_DELETE_THIS_FOLLOW_UP': 'Are you sure you want to delete this follow-up?',
+
+    'LNG_FOLLOW_UP_FIELD_LABEL_DATE': 'Date',
+    'LNG_FOLLOW_UP_FIELD_LABEL_STATUS_ID': 'Status',
+    'LNG_FOLLOW_UP_FIELD_LABEL_TARGETED': 'Targeted',
+
+
+    //casesScreen
+    'LNG_CASE_LIST_SCREEN_TITLE': 'Cases',
+    'LNG_CASE_LIST_SCREEN_VIEW_BUTTON': 'View',
+    'LNG_CASE_LIST_SCREEN_ADD_CONTACT_BUTTON': 'Add contact',
+
+
+    //contactsScreen
+    'LNG_CONTACT_LIST_SCREEN_TITLE': 'Contacts',
+    'LNG_CONTACT_LIST_SCREEN_ADD_FOLLOW_UP_BUTTON': 'Add follow-up',
+    'LNG_CONTACT_LIST_SCREEN_EDIT_BUTTON': 'Edit',
+
+
+    //followUpsScreen
+    'LNG_FOLLOW_UPS_LIST_SCREEN_TITLE': 'Follow-ups',
+    'LNG_FOLLOW_UPS_LIST_SCREEN_NO_FOLLOW_UPS_MESSAGE': 'There are no follow-ups to display',
+    'LNG_FOLLOW_UPS_LIST_SCREEN_ADD_EXPOSURE_BUTTON': 'Add exposure',
+
+    'LNG_PAGE_LIST_USAGE_LOCATIONS_TYPE_LABEL_FOLLOW_UP': 'Follow-up',
+    'LNG_PAGE_LIST_DUPLICATE_RECORDS_LABEL_MISSING': 'Missing',
+    'LNG_MESSAGE_EXPOSED_TO_LIST_ITEM': 'Exposed to',
+
+    //not used
+    'Generate for current day': 'Generate for current day',
+    'Generate for 1 day': 'Generate for 1 day',
+
+
+    //missedFollowUpListItemLabels
+    'Missed': 'Missed',
+    'Last follow-up': 'Last follow-up',
+    'Next follow-up': 'Next follow-up',
+
+
+    //followUpsScreenStatusFilterValues
+    'LNG_FOLLOW_UPS_DROP_DOWN_FILTER_STATUS_LABEL_ALL': 'All',
+    'LNG_REFERENCE_DATA_CONTACT_DAILY_FOLLOW_UP_STATUS_TYPE_NOT_PERFORMED': 'Not Performed',
+    'LNG_REFERENCE_DATA_CONTACT_DAILY_FOLLOW_UP_STATUS_TYPE_MISSED': 'Missed',
+
+
+    //alertMessages
+    // alert general
+    'LNG_DIALOG_CONFIRM_BUTTON_OK': 'Ok',
+    'LNG_DIALOG_CONFIRM_BUTTON_YES': 'Yes',
+    'LNG_DIALOG_CONFIRM_BUTTON_CANCEL': 'Cancel',
+    'LNG_DIALOG_ALERT_TITLE_LABEL': 'Alert',
+    'LNG_DIALOG_VALIDATION_ERROR_TITLE_LABEL': 'Validation error',
+    'LNG_DIALOG_WARNING_TITLE_LABEL': 'Warning',
+
+    //not used with translation
+    'Invalid credentials': 'Invalid credentials',
+    'Invalid email address': 'Invalid email address',
+    'Please make sure you have entered a valid email address': 'Please make sure you have entered a valid email address',
+    'No person exists with this information': 'No person exists with this information',
+    'QR code is not valid': 'QR code is not valid',
+
+    // cases
+    'LNG_DIALOG_ALERT_MESSAGE_ARE_YOU_SURE_YOU_WANT_TO_DELETE_THIS_CASE': 'Are you sure you want to delete this case?',
+    'LNG_DIALOG_ALERT_MESSAGE_ARE_YOU_SURE_YOU_WANT_TO_DISCARD_ALL_CHANGES': 'Are you sure you want to discard all changes?',
+
+    // contacts
+    'LNG_DIALOG_ALERT_MESSAGE_CANNOT_DELETE_CONTACT_LAST_EXPOSURE': 'Cannot delete the contact last exposure',
+    'LNG_DIALOG_ALERT_MESSAGE_ARE_YOU_SURE_YOU_WANT_TO_DELETE_THIS_EXPOSURE': 'The exposure will be removed from the database. Are you sure you want to delete this exposure?',
+
+    'LNG_DIALOG_ALERT_MESSAGE_DATE_OF_ISOLATION_SHOULD_NOT_BE_BEFORE_DATE_OF_ONSET': 'The date of isolation should not be before date of onset',
+    'LNG_DIALOG_ALERT_MESSAGE_DATE_OF_OUTCOME_SHOULD_NOT_BE_BEFORE_DATE_OF_ONSET': 'The date of outcome should not be before date of onset',
+    'LNG_DIALOG_ALERT_MESSAGE_NUMBER_OF_MONTHS_MUST_BE_BETWEEN_MESSAGE': 'Number of months must be between 0 and 11',
+    'LNG_DIALOG_ALERT_MESSAGE_NUMBER_OF_YEARS_MUST_BE_BETWEEN_MESSAGE': 'Number of years must be between 0 and 150',
+    'LNG_DIALOG_ALERT_MESSAGE_COMPLETE_ALL_MISSING_REQUIRED_FIELDS': 'Some of the required fields are missing. Please make sure you have completed them',
+    'LNG_DIALOG_ALERT_MESSAGE_THERE_WAS_AN_ISSUE_GETTING_YOUR_LOCATION': 'There was an issue with getting your location',
+    'LNG_DIALOG_ALERT_MESSAGE_MAKE_SURE_YOU_COMPLETE_ALL_FIELDS': 'Please make sure you have completed the fields',
+    'LNG_DIALOG_ALERT_MESSAGE_ERROR_OCCURED_WHILE_GETTING_CONTACTS': 'An error occurred while getting contacts',
+
+    // mapsPopup
+    'LNG_DIALOG_ALERT_MESSAGE_SELECT_MAPS_APP_THAT_YOU_WOULD_LIKE_TO_USER': 'Select the maps application that you would like to use',
+
+    // address
+    'LNG_DIALOG_ALERT_MESSAGE_PLEASE_ADD_THE_PLACE_OF_RESIDENCE_ADDRESS': 'Please add the place of residence address',
+    'LNG_DIALOG_ALERT_MESSAGE_ADD_AT_LEAST_ONE_ADDRESS_WITH_ALL_REQUERED_FIELDS_COMPLETED': 'Please add at least one address with all the required fields completed',
+
+
+    //loadingScreenMessages
+    'LNG_LOADING_SCREEN_MESSAGE': 'Loading...',
+
+    
+    //generalButtons
+    'LNG_COMMON_BUTTON_NEXT': 'Next',
+    'LNG_COMMON_BUTTON_BACK': 'Back',
+    'LNG_COMMON_BUTTON_CANCEL': 'Cancel',
+    'LNG_COMMON_BUTTON_EDIT': 'Edit',
+
+
+    //generalLabels
+    'LNG_LAYOUT_LIST_DEFAULT_FILTER_PLACEHOLDER': 'Filter By',
+    'LNG_COMMON_BUTTON_APPLY_FILTERS': 'Apply filters',
+    'LNG_COMMON_LABEL_YES': 'Yes',
+    'LNG_COMMON_LABEL_NO': 'No',
+    'LNG_COMMON_LABEL_TODAY': 'Today',
+    'LNG_SEARCH_LOCATIONS_AUTO_COMPLETE_TYPE_TO_SEARCH_TEXT': 'Type to search',
+
+    //take just first letter
+    'LNG_COMMON_LABEL_QUESTION': 'Question',
+
+
+    //questionCardLabels
+    'LNG_QUESTIONNAIRE_LABEL_WRITE_ANSWER': 'Write answer',
+    'LNG_QUESTIONNAIRE_LABEL_SELECT_DATE': 'Select date',
+    'LNG_QUESTIONNAIRE_LABEL_SELECT_ANSWER': 'Select answer',
+
+
+    //firstConfigScreen
+    'Welcome': 'Welcome',
+    'Choose one of the options below to sync with your HUB:': 'Choose one of the options below to sync with your HUB:',
+    'Scan QR Code': 'Scan QR Code',
+    'Config HUB Manually': 'Config HUB Manually',
+
+
+    //qrScanScreen
+    'LNG_SCAN_QR_CODE_SCREEN_TITLE': 'Scan QR code',
+
+
+    //manualConfigScreen
+    'HUB configuration': 'HUB configuration',
+    'HUB Name': 'HUB Name',
+    'HUB URL': 'HUB URL',
+    'Client ID': 'Client ID',
+    'Client secret': 'Client secret',
+    'Save HUB configuration': 'Save HUB configuration',
+    'Encrypted connection': 'Encrypted connection',
+
+
+    //loginScreen
+    'Welcome': 'Welcome',
+    'Email': 'Email',
+    'Password': 'Password',
+    'LOGIN': 'LOGIN',
+
+
+    //addressFieldLabels
+    'LNG_ADDRESS_FIELD_LABEL_NAME': 'Name',
+    'LNG_ADDRESS_FIELD_LABEL_DATE': 'Date',
+    'LNG_ADDRESS_FIELD_LABEL_LOCATION': 'Area',
+    'LNG_ADDRESS_FIELD_LABEL_ADDRESS_CITY': 'City',
+    'LNG_ADDRESS_FIELD_LABEL_ZIP': 'Zip',
+    'LNG_ADDRESS_FIELD_LABEL_ADDRESS': 'Address',
+    'LNG_ADDRESS_FIELD_LABEL_LONGITUDE': 'Longitude',
+    'LNG_ADDRESS_FIELD_LABEL_LATITUDE': 'Latitude',
+    'LNG_COMMON_BUTTON_DELETE': 'Delete',
+
+
+    //personTypes
+    'LNG_REFERENCE_DATA_CATEGORY_PERSON_TYPE_CASE': 'Case',
+    'LNG_REFERENCE_DATA_CATEGORY_PERSON_TYPE_CONTACT': 'Contact',
+    'LNG_REFERENCE_DATA_CATEGORY_PERSON_TYPE_EVENT': 'Event',
+
+
+    //userResidenceAddress
+    'LNG_REFERENCE_DATA_CATEGORY_ADDRESS_TYPE_USUAL_PLACE_OF_RESIDENCE': 'Current Address',
+    'LNG_REFERENCE_DATA_CATEGORY_ADDRESS_TYPE_PREVIOUS_USUAL_PLACE_OF_RESIDENCE': 'Previous Address',
+
+
+    //contactFollowUpStatuses
+    'LNG_REFERENCE_DATA_CONTACT_FINAL_FOLLOW_UP_STATUS_TYPE_UNDER_FOLLOW_UP': 'Under Follow-up',
+    'LNG_REFERENCE_DATA_CONTACT_FINAL_FOLLOW_UP_STATUS_TYPE_FOLLOW_UP_COMPLETED': 'Follow-up Completed',
+    'LNG_REFERENCE_DATA_CONTACT_FINAL_FOLLOW_UP_STATUS_TYPE_LOST_TO_FOLLOW_UP': 'Lost To Follow-up',
+
+
+    //followUpStatuses
+    'LNG_REFERENCE_DATA_CONTACT_DAILY_FOLLOW_UP_STATUS_TYPE_SEEN_OK': 'Seen Ok',
+    'LNG_REFERENCE_DATA_CONTACT_DAILY_FOLLOW_UP_STATUS_TYPE_SEEN_NOT_OK': 'Seen Not Ok',
+
+
+    //localTranslationTokens
+    'LNG_REFERENCE_DATA_CATEGORY_GENDER_MALE': 'Male',
+    'LNG_REFERENCE_DATA_CATEGORY_GENDER_FEMALE': 'Female',
+
+
+    //TextSwitchSelectorAgeOrDobValues
+    'LNG_CONTACT_FIELD_LABEL_AGE': 'Age',
+    'LNG_CONTACT_FIELD_LABEL_DATE_OF_BIRTH': 'Date of birth',
+
+
+    //ageUnitOfMeasureDropDown
+    'LNG_AGE_FIELD_LABEL_YEARS': 'Years',
+    'LNG_AGE_FIELD_LABEL_MONTHS': 'Months',
+
+
+    //dropDownLabels
+    'LNG_QUESTIONNAIRE_LABEL_SELECT_ANSWERS': 'Select answers',
+    'LNG_MULTIPLE_ANSWERS_NUMBER_OF_ANSWERS_SELECTED_END': 'answer(s)',
+    'LNG_MULTIPLE_ANSWERS_NUMBER_OF_ANSWERS_SELECTED_START': 'Selected',
+
+
+    //dropDownSectionedLabels
+    'LNG_ADD_ADDRESS_TAB_ADD_LOCATION_MESSAGE': 'Choose location',
+    'LNG_LOCATIONS_DROP_DOWN_SECTIONED_TITLE': 'Search regions',
+    'LNG_NO_LOCATIONS_AVAILABLE_MESSAGE': 'There were no elements found',
+
+
+    //hubConfigScreen
+    'LNG_HUB_CONFIGURATION_SCREEN_TITLE': 'HUB configuration',
+    'LNG_HUB_CONFIGURATION_SCREEN_CURRENT_HUB_CONFIG': 'Your current hub configuration is:',
+    'LNG_HUB_CONFIGURATION_SCREEN_CURRENT_CONFIG_NAME': 'Name',
+    'LNG_HUB_CONFIGURATION_SCREEN_CURRENT_CONFIG_URL': 'URL',
+    'LNG_HUB_CONFIGURATION_SCREEN_CURRENT_CONFIG_CLIENT_ID': 'Client ID',
+    'LNG_HUB_CONFIGURATION_SCREEN_CURRENT_CONFIG_CLIENT_SECRET': 'Client secret',
+    'LNG_HUB_CONFIGURATION_SCREEN_LAST_SYNC_DATE': 'Last sync date',
+    'LNG_HUB_CONFIGURATION_SCREEN_SCAN_QR_CODE_BUTTON': 'Scan QR Code',
+    'LNG_COMMON_BUTTON_SAVE': 'Save',
+    'LNG_HUB_CONFIGURATION_SCREEN_WANT_TO_CONNECT_TO_DIFFERENT_COMPUTER_LABEL': 'Do you want to connect to a different computer?',
+    'LNG_HUB_CONFIGURATION_SCREEN_ADD_HUB_BUTTON': 'Add HUB',
+    'LNG_HUB_CONFIGURATION_SCREEN_OTHER_HUB_CONFIGURATIONS_LABEL': 'Other hub configurations',
+    'LNG_HUB_CONFIGURATION_SCREEN_MAKE_ACTIVE_ACTION': 'Make Active',
+
+    // Errors
+    'LNG_HUB_CONFIGURATION_SCREEN_ERROR_SET_ACTIVE_HUB': 'Error set active HUB',
+    'LNG_HUB_CONFIGURATION_SCREEN_ERROR_OCCURED_WHILE_SETTING_ACTIVE_HUB': 'An error occurred while setting the new active HUB',
+
+    'LNG_HUB_CONFIGURATION_SCREEN_ERROR_SAVE_CURRENT_HUB': 'Error save current HUB',
+    'LNG_HUB_CONFIGURATION_SCREEN_ERROR_WHILE_SAVING_THE_UPDATE_HUB_INFO': 'An error occurred while saving the update HUB info',
+
+    'LNG_HUB_CONFIGURATION_SCREEN_ERROR_GET_HUBS': 'Error get HUBs',
+    'LNG_HUB_CONFIGURATION_SCREEN_ERROR_WHILE_GETTING_THE_OTHER_HUBS_CONFIG': 'An error occurred while getting the other HUBS configurations',
+
+    'LNG_HUB_CONFIGURATION_SCREEN_ERROR_GET_CURRENT_HUB': 'Error get current HUB',
+    'LNG_HUB_CONFIGURATION_SCREEN_ERROR_WHILE_GETTING_CURRENT_HUB_CONFIG': 'An error occurred while getting current HUB configuration',
+
+    'LNG_HUB_CONFIGURATION_SCREEN_ERROR_GET_LAST_SYNC_DATE': 'Error get last sync date',
+    'LNG_HUB_CONFIGURATION_SCREEN_ERROR_WHILE_GETTING_CURRENT_HUBS_LAST_SYNC_DATE': "An error occurred while getting current HUB's last sync date",
+
+    'LNG_HUB_CONFIGURATION_SCREEN_ERROR_YOU_HAVE_UNSAVED_DATA_SURE_LEAVE_PAGE': 'You have unsaved data. Are you sure you want to leave this page and lose all changes?',
+
+    // Success saving database
+    'LNG_HUB_CONFIGURATION_SCREEN_SUCCESS_UPDATING_HUB': '',
+    'LNG_HUB_CONFIGURATION_SCREEN_HUB_CREDENTIALS_SUCCESS_UPDATED': 'HUB credentials were successfully updated',
+
+    'LNG_HUB_CONFIGURATION_SCREEN_SUCCES_SET_HUB_AS_ACTIVE_AND_YOU_WILL_BE_LOGGED_OUT_MESSAGE': 'The HUB was successfully made as active. You will be logged out and can log in with the credentials for the new HUB configuration',
+};
