62c91c8efa99ef7cc4f1da691066b4d283fcf56e
florinpopa <florin.popa@clarisoft.com>
2020-05-26 12:37:13 +0300

    WGD-3156 - Created method to enhance fetch by adding timeout support and used it in the checkDevice for wipe method

--- src/requests/deviceStatus.js
+++ src/requests/deviceStatus.js
@@ -1,18 +1,20 @@
 import base64 from 'base-64';
 import {handleResponse} from './../utils/functions';
+import {fetchWitTimeout, retriablePromise} from './../utils/typeCheckingFunctions';
 
 export function checkDeviceStatus(url, installationId, clientId, clientSecret, callback) {
     if (url && installationId) {
         let requestUrl = `${url}/devices/find-by-physical-device-id/${installationId}`;
 
-        fetch( encodeURI(requestUrl), {
+        retriablePromise(fetchWitTimeout( encodeURI(requestUrl), {
             method: 'GET',
             headers: {
                 'Authorization': 'Basic ' + base64.encode(`${clientId}:${clientSecret}`),
                 'Accept': 'application/json',
                 'Content-Type': 'application/json',
             },
-        })
+            timeout: 2000
+        }),3, 100)
             .then((response) => {
                 return handleResponse(response);
             })
